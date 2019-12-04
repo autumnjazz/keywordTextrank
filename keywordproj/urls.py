4a0892d7ca57bcbc4355abcceff234c1a2dc52d2
foollbar <foollbar@gmail.com>
2019-12-05 00:06:16 +0900

    Add dummy static data

--- keywordproj/urls.py
+++ keywordproj/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 import showkeyword.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('',showkeyword.views.home, name="home"),
     path('count/',showkeyword.views.count, name="count"),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
