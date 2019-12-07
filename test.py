474cb594ceada5a06f25aedd3b46f59d9b9ce62d
autumnjazz <jandarea@naver.com>
2019-12-07 17:42:03 +0900

    Subgraph and subcommunitygraph

--- test.py
+++ test.py
@@ -16,10 +16,17 @@ mainkeywords = textrank_keyword(text, ibys, wordlist,numofkeys)
 
 import visualization as vis
 cnt_draw = vis.counter_draw(counter,wordlist)
-IG = vis.initialGraph(cnt_draw,wordlist) # returns a graph
-# vis.initialGraph_draw(IG)
-# TG = vis.textrankGraph(mainkeywords) # returns a graph
-# vis.textrankGraph_draw(TG)
-partition, color = vis.communityGraph(IG) # returns dictionary and list
-vis.communityGraph_draw(IG, color) # the variable 'color' has 'partiton' variable, which can color the nodes by community
+IG = vis.initialGraph(cnt_draw,wordlist)
+vis.drawgraph(IG, cmap = "Blues", nodesize = 350, graphtype = None, savepath="initial.png", show = False)
 
+TG = vis.textrankGraph(mainkeywords) 
+vis.drawgraph(TG, cmap = "Reds", nodesize = 350, graphtype = "textrank", savepath="textrank.png", show = False)
+
+vis.communityGraph(IG) 
+vis.drawgraph(IG, cmap = "Pastel1", nodesize = 350, graphtype = "community", savepath="community.png", show = False)
+
+energy_SG = vis.subGraph(IG, "energy")
+vis.drawgraph(energy_SG, cmap = "Oranges", nodesize = 350, graphtype = None, savepath="subgraph.png", show = False)
+
+energy_SCG = vis.subCommunityGraph(IG, "energy") #only after communityGraph() method
+vis.drawgraph(energy_SCG, cmap = "Pastel1", nodesize = 350, graphtype = "community", savepath="subcommunity.png", show = False)
--- visualization.py
+++ visualization.py
@@ -2,6 +2,9 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import community
 
+"""
+to initialize a graph
+"""
 def counter_draw(counter, wordlist):
     cnt_draw = []
     for edge, weight in counter.items(): #counter.items() = ((1,2),3)
@@ -13,50 +16,70 @@ def initialGraph(cnt_draw,wordlist):
     G.add_weighted_edges_from(cnt_draw) #automatically create nodes as words. no overlapped edges.
     return G
 
-def initialGraph_draw(G):
+"""
+to transfom a graph
+"""
+def textrankGraph(mainkeywords):
+    G = nx.Graph()
+    for keyw in mainkeywords:
+        G.add_node(keyw[0], weight=keyw[1])
+    return G
+
+def communityGraph(G):
+    CG = G
+    partition = community.best_partition(CG) # dictionary {'word1': (int)community1, 'word2': community2, ... }
+    nx.set_node_attributes(CG,partition,'comm')
+    return CG
+
+def subGraph(G,center):
+    SG = G.subgraph(G[center]).copy()
+    elist = []
+    for n in SG.nodes():
+        elist.append((n,center))
+    SG.add_edges_from(elist)
+    return SG
+
+def subCommunityGraph(G, center):
+    nodelist = []
+    info =nx.get_node_attributes(G,'comm')
+    commNumm = info[center]
+    for n in G:
+        if commNumm == info[n]:
+            nodelist.append(n)
+    SCG = G.subgraph(nodelist)
+    return SCG
+
+
+
+"""
+drawing methods with matplotlib
+"""
+
+def drawgraph(G, cmap = "Blues", nodesize = 350, graphtype = None, savepath=None, show = False):
+
+    if graphtype == "textrank":
+        color = [n[1] for n in G.nodes(data='weight')]
+        nodesize = [(n[1]%100)**2 * 200 for n in G.nodes(data='weight')]
+    elif graphtype == "community":
+        info =nx.get_node_attributes(G,'comm')
+        color = list(info.values())
+    else :
+        color = [len(G[n]) for n in G] #color node by num. of neighbors
+   
     nx.draw_networkx(   
         G,
         pos = nx.spring_layout(G),
         with_labels = True,
-        node_color=[len(G[n]) for n in G], #color node by num. of neighbors
-        cmap="Blues", 
-        node_size =300,
+        node_color=color, #color node by num. of neighbors
+        cmap=cmap, 
+        node_size =nodesize,
         font_size = 6,
         width = 0.4 
     ) 
-    plt.show()
-    # plt.savefig('initgraph',  bbox_inches ='tight', format='svg')
+    if show == True:
+        plt.show()
 
-def textrankGraph(mainkeywords):
-    G = nx.Graph()
-    for i,keyw in enumerate(mainkeywords):
-        G.add_node(keyw[0], weight=keyw[1])
-    return G
-
-def textrankGraph_draw(G):
-    nx.draw_networkx(   
-        G,
-        # pos = nx.get_node_attributes(G,'pos'),
-        with_labels = True,
-        node_color=[n[1] for n in G.nodes(data='weight')], 
-        cmap="Reds", 
-        node_size =[(n[1]%100)**2 * 100 for n in G.nodes(data='weight')],
-        font_size = 6,
-        width = 0.4 
-    )
-    plt.show()
-    # plt.savefig('textrankgraph',  bbox_inches ='tight', format='svg')
-
-
-def communityGraph(G):
-    partition = community.best_partition(G) # dictionary {'word1': (int)community1, 'word2': community2, ... }
-    colors = [partition.get(node) for node in G.nodes()]
-    # mod = community.modularity(partition,G) #modularity of community
-    return partition, colors
-
-def communityGraph_draw(G, colors):
-    pos = nx.spring_layout(G)
-    nx.draw_networkx_nodes(G, pos, node_size=300, node_color=colors, cmap='Pastel1')
-    nx.draw_networkx_edges(G, pos, width = 0.4)
-    nx.draw_networkx_labels(G, pos, font_size=6, font_color="black")
-    plt.show()
+    if savepath != None:
+        plt.savefig(savepath,  bbox_inches ='tight')
+        plt.close()
+        
